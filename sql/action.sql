062f12d8702ac7de5fc7c81f1608f587cb138a7c
Romain Thouvenin <romain@wemove.eu>
2019-10-16 17:52:35 +0200

    Adapt load queries to new action and campaign models

--- sql/action.sql
+++ sql/action.sql
@@ -1,16 +1,42 @@
--- regular donations action
+INSERT INTO action
+  (campaign_id, action_type, started_at, ended_at, external_id, external_system)
+  SELECT
+    camp.id,
+    'donate',
+    c.start_date,
+    c.end_date,
+    SUBSTR(c.external_identifier FROM 4),
+    'houdini'
+  FROM wemove_47.civicrm_campaign c
+  JOIN wemove_47.civicrm_campaign p ON p.id=c.parent_id
+  JOIN campaign camp ON camp.name=p.name COLLATE utf8_general_ci
+  WHERE c.external_identifier REGEXP '^cc_[0-9]+$'
+;
 
 INSERT INTO action
-(campaign_id, action_type, external_id)
-
-SELECT
-distinct
-c.id,
-'regular_donation',
-c.external_id
-
-FROM
-contribution_recur_to_campaign rd2c
-JOIN
-campaign c ON rd2c.campaign_id = c.id
+  (campaign_id, action_type, language, started_at, ended_at, external_id, external_system)
+  SELECT
+    camp.id,
+    CASE
+    WHEN activity_type_id = 2 THEN 'call'
+    WHEN activity_type_id = 3 THEN 'email'
+    WHEN activity_type_id = 32 THEN 'sign'
+    WHEN activity_type_id = 54 THEN 'share'
+    WHEN activity_type_id = 59 THEN 'tweet'
+    WHEN activity_type_id = 67 THEN 'facebook'
+    END AS action_type,
+    x.language_4,
+    c.start_date,
+    c.end_date,
+    c.external_identifier,
+    IF(c.external_identifier > 10000, 'youmove', 'speakout')
+  FROM wemove_47.civicrm_campaign c
+  JOIN wemove_47.civicrm_campaign p ON p.id=c.parent_id
+  JOIN campaign camp ON camp.name=p.name COLLATE utf8_general_ci
+  JOIN wemove_47.civicrm_activity a ON a.campaign_id=c.id
+  JOIN wemove_47.civicrm_value_speakout_integration_2 x ON x.entity_id=c.id
+  WHERE c.external_identifier NOT LIKE 'cc_%'
+    AND a.activity_type_id IN (2, 3, 32, 54, 59, 67)
+  GROUP BY c.id, camp.id, activity_type_id
 ;
+
--- sql/campaign.sql
+++ sql/campaign.sql
@@ -1,28 +1,22 @@
 INSERT INTO campaign 
-(name,  started_at,  campaign_type, external_system)
-VALUES
-('Other Fundraising', '2015-01-01', 'member', 'civicrm'), -- null medium
-('Survey Fundraising', '2015-01-01', 'member', 'civicrm'); -- utm_medium=dupal-survey
+  (name,  started_at,  campaign_type)
+  VALUES
+    ('Other Fundraising', '2015-01-01', 'wemove'), -- null medium
+    ('Survey Fundraising', '2015-01-01', 'wemove') -- utm_medium=dupal-survey
+;
 
-INSERT INTO
-campaign
-(name, parent_id, started_at, ended_at, language, campaign_type, external_id, external_system)
-
-SELECT
-c.name,
-NULL as parent_id,
-c.start_date,
-c.end_date,
-NULL as language,
-CASE
- WHEN c.campaign_type_id IN (1,2,3,4,5,7,8,11) THEN 'member'
- WHEN c.campaign_type_id IN (6) THEN 'distributed'
- WHEN c.campaign_type_id IN (9) THEN 'eci'
- WHEN c.campaign_type_id IN (10) THEN 'nomember'
-END as campaign_type,
-c.id as external_id, 'civicrm'
-
-FROM
-wemove_47.civicrm_campaign c
+INSERT INTO campaign
+  (name, started_at, ended_at, campaign_type)
+  SELECT
+    c.name,
+    c.start_date,
+    c.end_date,
+    CASE
+     WHEN c.campaign_type_id IN (1,2,3,4,5,7,8,11) THEN 'wemove'
+     WHEN c.campaign_type_id IN (6) THEN 'youmove'
+     WHEN c.campaign_type_id IN (9,10) THEN 'eci'
+    END as campaign_type
+  FROM wemove_47.civicrm_campaign c
+  WHERE c.id=c.parent_id
 ;
 
