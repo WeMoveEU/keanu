d602db6e89b8b047a2b81460525cb1d9319498b5
Romain Thouvenin <romain@wemove.eu>
2019-10-17 17:16:20 +0200

    Adapt load queries to model changes

--- sql/action.sql
+++ sql/action.sql
@@ -1,64 +1,24 @@
--- Houdini widgets
 INSERT INTO action
-  (campaign_id, action_type, started_at, ended_at, external_id, external_system)
-  SELECT
-    camp.id,
-    'donate',
-    c.start_date,
-    c.end_date,
-    RIGHT(c.external_identifier, 1),
-    'houdini'
-  FROM wemove_47.civicrm_campaign c
-  JOIN wemove_47.civicrm_campaign p ON p.id=c.parent_id
-  JOIN campaign camp ON camp.name=p.name COLLATE utf8_general_ci
-  WHERE c.external_identifier REGEXP '^cc[a-z_]+[0-9]+$'
-;
-
--- Speakout actions
-INSERT INTO action
-  (campaign_id, action_type, language, started_at, ended_at, external_id, external_system)
+  (campaign_id, action_type, language, external_id, external_system)
   SELECT
     camp.id,
     CASE
     WHEN activity_type_id = 2 THEN 'call'
     WHEN activity_type_id = 3 THEN 'email'
+    WHEN activity_type_id = 6 THEN 'donate'
     WHEN activity_type_id = 32 THEN 'sign'
     WHEN activity_type_id = 54 THEN 'share'
     WHEN activity_type_id = 59 THEN 'tweet'
     WHEN activity_type_id = 67 THEN 'facebook'
     END AS action_type,
     x.language_4,
-    c.start_date,
-    c.end_date,
-    c.external_identifier,
-    IF(c.external_identifier > 10000, 'youmove', 'speakout')
+    c.id,
+    'civicrm_campaign'
   FROM wemove_47.civicrm_campaign c
   JOIN wemove_47.civicrm_campaign p ON p.id=c.parent_id
-  JOIN campaign camp ON camp.name=p.name COLLATE utf8_general_ci
+  JOIN campaign camp ON camp.external_id=p.id
   JOIN wemove_47.civicrm_activity a ON a.campaign_id=c.id
   JOIN wemove_47.civicrm_value_speakout_integration_2 x ON x.entity_id=c.id
-  WHERE c.external_identifier NOT LIKE 'cc_%'
-    AND a.activity_type_id IN (2, 3, 32, 54, 59, 67)
+  WHERE a.activity_type_id IN (2, 3, 6, 32, 54, 59, 67)
   GROUP BY c.id, camp.id, activity_type_id
 ;
-
--- CiviCRM contribution pages
-INSERT INTO action
-  (campaign_id, action_type, language, started_at, ended_at, external_id, external_system)
-  SELECT
-    camp.id,
-    'donate',
-    x.language_4,
-    IFNULL(dp.start_date, dp.created_date),
-    dp.end_date,
-    dp.id,
-    'civicrm_contrib'
-  FROM wemove_47.civicrm_contribution d
-  JOIN wemove_47.civicrm_contribution_page dp ON dp.id=d.contribution_page_id
-  JOIN wemove_47.civicrm_campaign c ON c.id=dp.campaign_id
-  JOIN wemove_47.civicrm_campaign p ON p.id=c.parent_id
-  JOIN campaign camp ON camp.name=p.name COLLATE utf8_general_ci
-  JOIN wemove_47.civicrm_value_speakout_integration_2 x ON x.entity_id=c.id
-  GROUP BY d.contribution_page_id, camp.id
-;
-
--- sql/campaign.sql
+++ sql/campaign.sql
@@ -1,5 +1,5 @@
 INSERT INTO campaign
-  (name, started_at, ended_at, campaign_type)
+  (name, started_at, ended_at, campaign_type, external_id, external_system)
   SELECT
     c.name,
     c.start_date,
@@ -8,7 +8,9 @@ INSERT INTO campaign
      WHEN c.campaign_type_id IN (1,2,3,4,5,7,8,11) THEN 'wemove'
      WHEN c.campaign_type_id IN (6) THEN 'youmove'
      WHEN c.campaign_type_id IN (9,10) THEN 'eci'
-    END as campaign_type
+    END as campaign_type,
+    c.id,
+    'civicrm_campaign'
   FROM wemove_47.civicrm_campaign c
   WHERE c.id=c.parent_id
 ;
