5757d5f6ae01243911e1f1b862bd115fa03623da
Romain Thouvenin <romain@wemove.eu>
2019-10-23 16:08:43 +0200

    fix views.sql

--- sql/views.sql
+++ sql/views.sql
@@ -1,36 +1,35 @@
 -- Use this view to figure out which campaign id is assigned to a Civi CONTRIB RECUR id
-DROP VIEW contribution_recur_to_campaign;
+DROP VIEW IF EXISTS contribution_recur_to_campaign;
 
 CREATE VIEW contribution_recur_to_campaign AS
-SELECT
-rd.id,
-CASE
-  WHEN c.id IS NULL THEN
-   CASE
-    WHEN utm.utm_medium = 'drupal-survey' THEN
-     (SELECT id from campaign where name = 'Survey Fundraising')
-    ELSE
-     (SELECT id from campaign where name = 'Unknown Fundraising')
-   END
-  ELSE c.id
-END as campaign_id,
-m.id as mailing_id,
-utm.utm_source,
-utm.utm_medium,
-utm.utm_campaign
+  SELECT
+    rd.id,
+    CASE
+      WHEN c.id IS NULL THEN
+       CASE
+        WHEN utm.utm_medium = 'drupal-survey' THEN
+         (SELECT id from campaign where name = 'Survey Fundraising')
+        ELSE
+         (SELECT id from campaign where name = 'Unknown Fundraising')
+       END
+      ELSE c.id
+    END as campaign_id,
+    m.id as mailing_id,
+    utm.utm_source,
+    utm.utm_medium,
+    utm.utm_campaign
 
--- useful join for getting campaign by the utm-mailing 
+  -- useful join for getting campaign by the utm-mailing
 
-FROM wemove_47.civicrm_contribution_recur rd
-LEFT JOIN wemove_47.civicrm_value_recur_utm utm
-  ON utm.entity_id = rd.id
-LEFT JOIN wemove_47.civicrm_mailing m
-  ON (utm.utm_medium = 'email' AND
-     substring_index(utm.utm_source, '-', -1) = m.id)
-LEFT JOIN wemove_47.civicrm_campaign c_speakout 
-  OR (utm.utm_medium = 'speakout' AND
-     substring_index(utm.utm_source, '-', -1) = c_speakout.external_identifier)
-LEFT JOIN campaign c
-  ON c.external_id = COALESCE(m.campaign_id, c_speakout.id)
+  FROM wemove_47.civicrm_contribution_recur rd
+  LEFT JOIN wemove_47.civicrm_value_recur_utm utm
+    ON utm.entity_id = rd.id
+  LEFT JOIN wemove_47.civicrm_mailing m
+    ON (utm.utm_medium = 'email' AND SUBSTRING(utm.utm_source, 10) = m.id)
+  LEFT JOIN wemove_47.civicrm_campaign c_speakout
+    ON (utm.utm_medium = 'speakout' AND SUBSTRING(utm.utm_source, 10) = c_speakout.external_identifier)
+  LEFT JOIN campaign c
+    ON c.external_id = COALESCE(m.campaign_id, c_speakout.id)
 
-WHERE rd.is_test = 0
+  WHERE rd.is_test = 0
+;
