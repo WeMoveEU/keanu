464e1414850b5aca41161a6587bebad250c68b06
Romain Thouvenin <romain@wemove.eu>
2019-10-23 17:26:20 +0200

    Loading of contact_action for one-off donations

--- sql/contact_action.sql
+++ sql/contact_action.sql
@@ -1,12 +1,33 @@
+-- one-off donate actions
+INSERT INTO contact_action
+  (contact_id, created_at, action_id, external_id, external_system)
+
+  SELECT
+    d.contact_id,
+    d.receive_date,
+    a.id,
+    d.id,
+    'civicrm_contribution'
+
+  FROM wemove_47.civicrm_contribution d
+  JOIN contact c ON c.id=d.contact_id -- to discard deleted contacts
+  JOIN wemove_47.civicrm_value_speakout_integration_2 x ON x.entity_id=d.campaign_id
+  JOIN action a
+    ON a.action_type='donate'
+    AND a.external_id = d.campaign_id
+    AND a.language = x.language_4 COLLATE utf8_general_ci
+  WHERE NOT d.is_test
+    AND d.contribution_recur_id IS NULL
+;
+
 -- recurring donation action
 INSERT INTO contact_action
-  (contact_id, created_at, action_id, source_id, external_id, external_system)
+  (contact_id, created_at, action_id, external_id, external_system)
 
   SELECT
     rd.contact_id,
     rd.create_date,
     a.id as action_id,
-    null,
     rd.id,
     'civicrm_contribution_recur'
 
