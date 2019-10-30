3c574781f5b76198088c866991e3bebc84e5611a
Romain Thouvenin <romain@wemove.eu>
2019-10-30 14:19:07 +0100

    Add 'if exists' to function drops

--- sql/prepare/procedures.sql
+++ sql/prepare/procedures.sql
@@ -1,25 +1,25 @@
-DROP FUNCTION date_trunc_day;
+DROP FUNCTION IF EXISTS date_trunc_day;
 
 CREATE FUNCTION date_trunc_day (d DATETIME)
 RETURNS DATETIME DETERMINISTIC
 RETURN date(d);
 
 
-DROP FUNCTION date_trunc_month;
+DROP FUNCTION IF EXISTS date_trunc_month;
 
 CREATE FUNCTION date_trunc_month (d DATETIME)
 RETURNS DATETIME DETERMINISTIC
 RETURN str_to_date(CONCAT(date_format(d, '%Y-%m'), '-01') , '%Y-%m-%d');
 
 
-DROP FUNCTION date_trunc_quarter;
+DROP FUNCTION IF EXISTS date_trunc_quarter;
 
 CREATE FUNCTION date_trunc_quarter (d DATETIME)
 RETURNS DATETIME DETERMINISTIC
 RETURN str_to_date(CONCAT(date_format(d, '%Y-'), (quarter(d)-1)*3+1, '-01') , '%Y-%m-%d');
 
 
-DROP FUNCTION date_trunc_year;
+DROP FUNCTION IF EXISTS date_trunc_year;
 
 CREATE FUNCTION date_trunc_year (d DATETIME)
 RETURNS DATETIME DETERMINISTIC
