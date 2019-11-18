bf7e633d6b532f511f78169a8f4dcdad836bf925
Romain Thouvenin <romain@wemove.eu>
2019-11-18 10:18:20 +0000

    Donation model

--- sql/action.sql
+++ sql/action.sql
@@ -1,11 +1,10 @@
 INSERT INTO action
-    (campaign_id, action_type, language, external_id, external_system)
-SELECT
+  (campaign_id, action_type, language, external_id, external_system)
+  SELECT
     camp.id,
     CASE
     WHEN activity_type_id = 2 THEN 'call'
     WHEN activity_type_id = 3 THEN 'email'
-    WHEN activity_type_id = 6 THEN 'donate'
     WHEN activity_type_id = 32 THEN 'sign'
     WHEN activity_type_id = 54 THEN 'share'
     WHEN activity_type_id = 59 THEN 'tweet'
@@ -14,26 +13,39 @@ SELECT
     x.language_4,
     c.id,
     'civicrm_campaign'
-FROM wemove_47.civicrm_campaign c
-    JOIN wemove_47.civicrm_campaign p ON p.id=c.parent_id
-    JOIN campaign camp ON camp.external_id=p.id
-    JOIN wemove_47.civicrm_activity a ON a.campaign_id=c.id
-    JOIN wemove_47.civicrm_value_speakout_integration_2 x ON x.entity_id=c.id
-WHERE a.activity_type_id IN (2, 3, 6, 32, 54, 59, 67)
-GROUP BY c.id, camp.id, activity_type_id
-    ;
 
+  FROM wemove_47.civicrm_campaign c
+  JOIN wemove_47.civicrm_campaign p ON p.id=c.parent_id
+  JOIN campaign camp ON camp.external_id=p.id
+  JOIN wemove_47.civicrm_activity a ON a.campaign_id=c.id
+  JOIN wemove_47.civicrm_value_speakout_integration_2 x ON x.entity_id=c.id
 
--- Create regular_donation action for every Contribution Recur that cant be tracked by utm
--- the contribution_recur_to_campaign VIEW holds the logic 
-INSERT INTO action
-    (campaign_id, action_type)
-SELECT
-    distinct
-    c.id,
-    'regular_donation'
-FROM
-    contribution_recur_to_campaign rd2c
-    JOIN
-    campaign c ON rd2c.campaign_id = c.id
-    ;
+  WHERE a.activity_type_id IN (2, 3, 32, 54, 59, 67)
+  GROUP BY c.id, camp.id, activity_type_id
+;
+
+-- Create donate action for every contribution
+INSERT IGNORE INTO action
+  (campaign_id, action_type, language, external_id, external_system)
+  SELECT
+    camp.id, 'donate', x.language_4, c.id, 'civicrm_campaign'
+  FROM wemove_47.civicrm_campaign c
+  JOIN wemove_47.civicrm_campaign p ON p.id=c.parent_id
+  JOIN campaign camp ON camp.external_id=p.id
+  JOIN wemove_47.civicrm_contribution d ON d.campaign_id=c.id
+  JOIN wemove_47.civicrm_value_speakout_integration_2 x ON x.entity_id=c.id
+  GROUP BY c.id, camp.id
+;
+
+-- Create donate action for every recurring contribution
+INSERT IGNORE INTO action
+  (campaign_id, action_type, language, external_id, external_system)
+  SELECT
+    camp.id, 'donate', x.language_4, c.id, 'civicrm_campaign'
+  FROM wemove_47.civicrm_campaign c
+  JOIN wemove_47.civicrm_campaign p ON p.id=c.parent_id
+  JOIN campaign camp ON camp.external_id=p.id
+  JOIN wemove_47.civicrm_contribution_recur d ON d.campaign_id=c.id
+  JOIN wemove_47.civicrm_value_speakout_integration_2 x ON x.entity_id=c.id
+  GROUP BY c.id, camp.id
+;
