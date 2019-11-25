84b1b9d16a24ede6d5ebc07774ac429830025923
Marcin Koziej <marcin@cahoots.pl>
2019-11-25 14:25:23 +0100

    Rename action to action_page and contact_action to action

--- sql/donation.sql
+++ sql/donation.sql
@@ -56,7 +56,7 @@ CREATE INDEX all_contributions_external_ids ON all_contributions (external_id, e
 -- Insert donations both one-off and recurring in one go
 -- Use DISTINCT to get recurring donation just once
 INSERT INTO donation (
-        contact_action_id,
+        action_id,
         amount, total_amount, original_amount, original_currency,
         started_at, ended_at, payment_method,
         frequency_unit, frequency_interval,
@@ -80,11 +80,11 @@ SELECT DISTINCT
     ac.external_id,
     ac.external_system
 FROM all_contributions ac
-    JOIN contact_action ca ON ca.external_id = ac.external_id AND ca.external_system = ac.external_system
+    JOIN action ca ON ca.external_id = ac.external_id AND ca.external_system = ac.external_system
 
 -- BEGIN INCREMENTAL
--- exclude by contact_action references in donation table
-WHERE ca.id NOT IN (SELECT contact_action_id FROM donation)
+-- exclude by action references in donation table
+WHERE ca.id NOT IN (SELECT action_id FROM donation)
 -- END INCREMENTAL
     ;
 
