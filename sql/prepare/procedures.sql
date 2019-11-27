13df6c048ec1ce4a48ec23a14833836eb617c9e5
Romain Thouvenin <romain@wemove.eu>
2019-11-27 10:36:50 +0100

    Load sources from mailing links

--- sql/prepare/procedures.sql
+++ sql/prepare/procedures.sql
@@ -25,3 +25,29 @@ DROP FUNCTION IF EXISTS date_trunc_year;
 CREATE FUNCTION date_trunc_year (d DATETIME)
 RETURNS DATETIME DETERMINISTIC
 RETURN str_to_date(CONCAT(date_format(d, '%Y-'), '01-01') , '%Y-%m-%d');
+
+
+DROP FUNCTION IF EXISTS query_param;
+DELIMITER //
+CREATE FUNCTION query_param (url text, param char(32))
+RETURNS TEXT DETERMINISTIC
+BEGIN
+  DECLARE param_pos INT;
+  DECLARE param_length INT;
+  DECLARE param_end_pos INT;
+  SET param_pos = LOCATE(param, url);
+
+  IF param_pos = 0 THEN
+    RETURN NULL;
+  ELSE
+    SET param_end_pos = LOCATE('&', url, param_pos);
+    SET param_length = LENGTH(param);
+    IF param_end_pos = 0 THEN
+      RETURN SUBSTRING(url, param_pos + param_length + 1);
+    ELSE
+      RETURN SUBSTRING(url, param_pos + param_length + 1, param_end_pos - param_pos - param_length - 1);
+    END IF;
+  END IF;
+END
+//
+DELIMITER ;
--- sql/source.sql
+++ sql/source.sql
@@ -1,6 +1,17 @@
 -- ORDER: 20
 -- DELETE FROM source
+
+-- Activities
 INSERT IGNORE INTO source (campaign, source, medium)
   SELECT campaign_26, source_27, media_28
   FROM ${SOURCE}.civicrm_value_action_source_4
+;
+
+-- Mailing links
+INSERT IGNORE INTO source (source, medium, campaign)
+  SELECT
+    query_param(url, 'utm_source'), query_param(url, 'utm_medium'), query_param(url, 'utm_campaign')
+  FROM ${SOURCE}.civicrm_mailing_trackable_url u
+  JOIN ${SOURCE}.civicrm_mailing m ON m.id=u.mailing_id
+  WHERE m.scheduled_date IS NOT NULL
 ;
