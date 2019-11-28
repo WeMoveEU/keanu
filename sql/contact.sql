772f8a408b47e15b57196b1e40475e1e02a9bf69
Romain Thouvenin <romain@wemove.eu>
2019-11-28 17:21:24 +0100

    Do load soft-deleted contacts #7

--- sql/contact.sql
+++ sql/contact.sql
@@ -7,20 +7,20 @@
 INSERT INTO contact
   (
     id,
-   email_domain,
-   created_at,
-   postal_code,
-   country,
-   preferred_language,
-   latitude, longitude
+    email_domain,
+    created_at,
+    postal_code,
+    country,
+    preferred_language,
+    latitude, longitude
   )
 
   SELECT
     c.id,
-    MIN(substring(e.email FROM locate('@', e.email) + 1)),
+    MIN(SUBSTRING(e.email FROM LOCATE('@', e.email) + 1)),
     COALESCE(c.created_date, c.modified_date),
-    substr(MIN(a.postal_code), 1, 10),
-    lower(MIN(ctr.iso_code)),
+    SUBSTR(MIN(a.postal_code), 1, 10),
+    LOWER(MIN(ctr.iso_code)),
     c.preferred_language,
     MIN(a.geo_code_1), MIN(a.geo_code_2)
 
@@ -29,9 +29,8 @@ INSERT INTO contact
   LEFT JOIN ${SOURCE}.civicrm_address a ON a.contact_id = c.id AND a.is_primary
   LEFT JOIN ${SOURCE}.civicrm_country ctr ON ctr.id = a.country_id
 
-  WHERE NOT c.is_deleted
 -- BEGIN INCREMENTAL
-  AND c.id NOT IN (SELECT id FROM contact)
+  WHERE c.id NOT IN (SELECT id FROM contact)
 -- END INCREMENTAL
 
   GROUP BY c.id
