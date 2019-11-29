3badbfa6126a5d681e50c93d5d0964a6958df1ea
Romain Thouvenin <romain@wemove.eu>
2019-11-29 10:10:46 +0100

    Actions from activities

--- sql/action.sql
+++ sql/action.sql
@@ -42,4 +42,24 @@ INSERT INTO action
       AND rd.id NOT IN (SELECT external_id FROM action WHERE external_system = 'civicrm_contribution_recur')
   -- END INCREMENTAL
 
+-- Activities
+INSERT INTO action
+  (contact_id, created_at, action_page_id, external_id, external_system)
+
+  SELECT
+    ac.contact_id, activity_date_time, ap.id, a.id, 'civicrm_activity'
+  FROM ${SOURCE}.civicrm_activity a
+  JOIN ${SOURCE}.civicrm_activity_contact ac ON ac.activity_id = a.id AND ac.record_type_id = 2
+  JOIN action_page ap ON ap.external_id = a.campaign_id AND ap.external_system = 'civicrm_campaign' AND ap.action_type = CASE
+      WHEN activity_type_id = 2 THEN 'call'
+      WHEN activity_type_id = 3 THEN 'email'
+      WHEN activity_type_id = 32 THEN 'sign'
+      WHEN activity_type_id = 54 THEN 'share'
+      WHEN activity_type_id = 59 THEN 'tweet'
+      WHEN activity_type_id = 67 THEN 'facebook'
+    END
+  WHERE a.activity_type_id IN (2, 3, 32, 54, 59, 67)
+  -- BEGIN INCREMENTAL
+    AND a.id NOT IN (SELECT external_id FROM action WHERE external_system = 'civicrm_activity')
+  -- END INCREMENTAL
 ;
