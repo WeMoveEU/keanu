5f6e6a5a706695e91d93d6abbe5eca48dee64c12
Romain Thouvenin <romain@wemove.eu>
2019-11-29 10:28:14 +0100

    action sources

--- sql/action.sql
+++ sql/action.sql
@@ -5,49 +5,58 @@ SELECT @unattributed_donations := a.id FROM action_page a JOIN campaign c ON a.c
 
 -- one-off donate actions
 INSERT INTO action
-  (contact_id, created_at, action_page_id, external_id, external_system)
+  (contact_id, created_at, action_page_id, source_id, external_id, external_system)
 
   SELECT
     d.contact_id,
     d.receive_date,
     COALESCE(a.id, @unattributed_donations),
+    s.id,
     d.id,
     'civicrm_contribution'
 
   FROM ${SOURCE}.civicrm_contribution d
-  JOIN contact c ON c.id=d.contact_id -- to discard deleted contacts
   LEFT JOIN action_page a ON a.action_type='donate' AND a.external_id = d.campaign_id AND a.external_system = 'civicrm_campaign'
-WHERE NOT d.is_test AND d.contribution_recur_id IS NULL
+  LEFT JOIN ${SOURCE}.civicrm_value_utm_5 utm ON utm.entity_id = d.id
+  LEFT JOIN source s ON s.source = utm.utm_source_30 COLLATE utf8_general_ci
+                    AND s.medium = utm.utm_medium_31 COLLATE utf8_general_ci
+                    AND s.campaign = utm.utm_campaign_33 COLLATE utf8_general_ci
+  WHERE NOT d.is_test AND d.contribution_recur_id IS NULL
 -- BEGIN INCREMENTAL
-AND d.id NOT IN (SELECT external_id FROM action WHERE external_system = 'civicrm_contribution')
+  AND d.id NOT IN (SELECT external_id FROM action WHERE external_system = 'civicrm_contribution')
 -- END INCREMENTAL
 ;
 
 -- recurring donation action
 INSERT INTO action
-  (contact_id, created_at, action_page_id, external_id, external_system)
+  (contact_id, created_at, action_page_id, source_id, external_id, external_system)
 
   SELECT
     rd.contact_id,
     rd.create_date,
     COALESCE(a.id, @unattributed_donations),
+    s.id,
     rd.id,
     'civicrm_contribution_recur'
 
   FROM ${SOURCE}.civicrm_contribution_recur rd
-  JOIN contact c ON c.id=rd.contact_id -- to discard deleted contacts
-  JOIN action_page a ON a.action_type='donate' AND a.external_id = rd.campaign_id AND a.external_system = 'civicrm_campaign'
+  LEFT JOIN action_page a ON a.action_type='donate' AND a.external_id = rd.campaign_id AND a.external_system = 'civicrm_campaign'
+  LEFT JOIN ${SOURCE}.civicrm_value_recur_utm utm ON utm.entity_id = rd.id
+  LEFT JOIN source s ON s.source = utm.utm_source COLLATE utf8_general_ci
+                    AND s.medium = utm.utm_medium COLLATE utf8_general_ci
+                    AND s.campaign = utm.utm_campaign COLLATE utf8_general_ci
   WHERE NOT rd.is_test
   -- BEGIN INCREMENTAL
-      AND rd.id NOT IN (SELECT external_id FROM action WHERE external_system = 'civicrm_contribution_recur')
+    AND rd.id NOT IN (SELECT external_id FROM action WHERE external_system = 'civicrm_contribution_recur')
   -- END INCREMENTAL
+;
 
 -- Activities
 INSERT INTO action
-  (contact_id, created_at, action_page_id, external_id, external_system)
+  (contact_id, created_at, action_page_id, source_id, external_id, external_system)
 
   SELECT
-    ac.contact_id, activity_date_time, ap.id, a.id, 'civicrm_activity'
+    ac.contact_id, activity_date_time, ap.id, s.id, a.id, 'civicrm_activity'
   FROM ${SOURCE}.civicrm_activity a
   JOIN ${SOURCE}.civicrm_activity_contact ac ON ac.activity_id = a.id AND ac.record_type_id = 2
   JOIN action_page ap ON ap.external_id = a.campaign_id AND ap.external_system = 'civicrm_campaign' AND ap.action_type = CASE
@@ -58,6 +67,10 @@ INSERT INTO action
       WHEN activity_type_id = 59 THEN 'tweet'
       WHEN activity_type_id = 67 THEN 'facebook'
     END
+  LEFT JOIN ${SOURCE}.civicrm_value_action_source_4 utm ON utm.entity_id = a.id
+  LEFT JOIN source s ON s.source = utm.source_27 COLLATE utf8_general_ci
+                    AND s.medium = utm.media_28 COLLATE utf8_general_ci
+                    AND s.campaign = utm.campaign_26 COLLATE utf8_general_ci
   WHERE a.activity_type_id IN (2, 3, 32, 54, 59, 67)
   -- BEGIN INCREMENTAL
     AND a.id NOT IN (SELECT external_id FROM action WHERE external_system = 'civicrm_activity')
