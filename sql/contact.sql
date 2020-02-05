e880936e3791bbc15ec496f97d005381174cdc67
Marcin Koziej <marcin@cahoots.pl>
2020-02-05 19:08:48 +0100

    Load contact - update changed existing contacts in incremental mode

--- sql/contact.sql
+++ sql/contact.sql
@@ -8,6 +8,25 @@
 SET @last_contact = (SELECT MAX(id) FROM contact);
 -- END INCREMENTAL
 
+DROP VIEW IF EXISTS civicrm_contact_to_contact;
+CREATE VIEW civicrm_contact_to_contact AS
+SELECT
+  c.id,
+  MIN(SUBSTRING(e.email FROM LOCATE('@', e.email) + 1)) as email_domain,
+  COALESCE(c.created_date, c.modified_date) as created_at,
+  SUBSTR(MIN(a.postal_code), 1, 10) as postal_code,
+  LOWER(MIN(ctr.iso_code)) as country,
+  c.preferred_language,
+  MIN(a.geo_code_1) as latitude,
+  MIN(a.geo_code_2) as longitude
+FROM ${SOURCE}.civicrm_contact c 
+LEFT JOIN ${SOURCE}.civicrm_email e ON e.contact_id = c.id AND e.is_primary
+LEFT JOIN ${SOURCE}.civicrm_address a ON a.contact_id = c.id AND a.is_primary
+LEFT JOIN ${SOURCE}.civicrm_country ctr ON ctr.id = a.country_id
+GROUP BY c.id
+;
+
+
 INSERT INTO contact
   (
     id,
@@ -18,27 +37,34 @@ INSERT INTO contact
     preferred_language,
     latitude, longitude
   )
-
-  SELECT
-    c.id,
-    MIN(SUBSTRING(e.email FROM LOCATE('@', e.email) + 1)),
-    COALESCE(c.created_date, c.modified_date),
-    SUBSTR(MIN(a.postal_code), 1, 10),
-    LOWER(MIN(ctr.iso_code)),
-    c.preferred_language,
-    MIN(a.geo_code_1), MIN(a.geo_code_2)
-
-  FROM ${SOURCE}.civicrm_contact c 
-  LEFT JOIN ${SOURCE}.civicrm_email e ON e.contact_id = c.id AND e.is_primary
-  LEFT JOIN ${SOURCE}.civicrm_address a ON a.contact_id = c.id AND a.is_primary
-  LEFT JOIN ${SOURCE}.civicrm_country ctr ON ctr.id = a.country_id
+SELECT *
+FROM civicrm_contact_to_contact c
 
 -- BEGIN INCREMENTAL
   WHERE c.id > @last_contact
 -- END INCREMENTAL
-
-  GROUP BY c.id
 ;
+
+-- BEGIN INCREMENTAL
+UPDATE contact dc
+JOIN civicrm_contact_to_contact sc ON dc.id = sc.id
+SET
+ dc.email_domain = sc.email_domain,
+ dc.postal_code = sc.postal_code,
+ dc.country = sc.country,
+ dc.preferred_language = sc.preferred_language,
+ dc.latitude = sc.latitude,
+ dc.longitude = sc.longitude
+WHERE
+ dc.email_domain != sc.email_domain  COLLATE utf8_general_ci OR
+ dc.postal_code != sc.postal_code  COLLATE utf8_general_ci OR
+ dc.country != sc.country  COLLATE utf8_general_ci OR
+ dc.preferred_language != sc.preferred_language  COLLATE utf8_general_ci OR
+ dc.latitude != sc.latitude OR
+ dc.longitude != sc.longitude
+;
+-- END INCREMENTAL
+
 
 -- Use MIN(created_date, MIN(activity_date_time)) as contact creation date,
 -- as a contact record may be created much later than the first activity date, in some ciscurmstances.
@@ -48,6 +74,9 @@ UPDATE contact c
     SELECT contact_id, MIN(activity_date_time) AS date_time
       FROM ${SOURCE}.civicrm_activity_contact ac
       JOIN ${SOURCE}.civicrm_activity a ON a.id = ac.activity_id AND ac.record_type_id = 2
+-- BEGIN INCREMENTAL
+      WHERE contact_id > @last_contact
+-- END INCREMENTAL
       GROUP BY contact_id
   ) min_act ON min_act.contact_id = c.id
   SET c.created_at = min_act.date_time
@@ -56,3 +85,5 @@ UPDATE contact c
     AND c.id > @last_contact
 -- END INCREMENTAL
 ;
+
+DROP VIEW IF EXISTS civicrm_contact_to_contact;
