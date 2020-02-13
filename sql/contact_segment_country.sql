7a8b38cb0cc3587a6bcc6d125cdac32a9d9396bf
Marcin Koziej <marcin@cahoots.pl>
2020-02-13 13:38:10 +0100

    Update countries segments using hot_contact

--- sql/contact_segment_country.sql
+++ sql/contact_segment_country.sql
@@ -2,7 +2,11 @@
 -- DELETE cs FROM contact_segment cs JOIN segment s ON cs.segment_id = s.id JOIN segmentation sn ON sn.id = s.segmentation_id WHERE sn.name = 'Country'
 
 -- BEGIN INCREMENTAL
-DELETE cs FROM contact_segment cs JOIN segment s ON cs.segment_id = s.id JOIN segmentation sn ON sn.id = s.segmentation_id WHERE sn.name = 'Country';
+DELETE cs FROM contact_segment cs
+          JOIN segment s ON cs.segment_id = s.id
+          JOIN segmentation sn ON sn.id = s.segmentation_id WHERE sn.name = 'Country'
+          JOIN hot_contact hc ON cs.contact_id = hc.id AND hc.modified
+;
 -- END INCREMENTAL
 
 INSERT INTO contact_segment -- Country
@@ -11,8 +15,12 @@ SELECT
 s.segmentation_id, s.id,
 c.id, c.created_at, NULL
 FROM
-segmentation sn JOIN segment s ON s.segmentation_id = sn.id
-JOIN ${SOURCE}.civicrm_country ctr ON s.external_id = ctr.id
-JOIN contact c ON c.country = ctr.iso_code COLLATE utf8_general_ci
-WHERE sn.name = 'Country'
+contact c
+-- BEGIN INCREMENTAL
+JOIN hot_contact hc ON cs.contact_id = hc.id AND hc.modified
+-- END INCREMENTAL
+JOIN ${SOURCE}.civicrm_country ctr ON c.country = ctr.iso_code COLLATE utf8_general_ci
+
+JOIN segment s ON ${SOURCE}.civicrm_country ctr ON s.external_id = ctr.id
+JOIN segmentation sn ON s.segmentation_id = sn.id AND sn.name = 'Country'
 ;
