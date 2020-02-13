509c5f2f7d4ea95771970b5e8af929fb248b1188
Marcin Koziej <marcin@cahoots.pl>
2020-02-13 13:45:50 +0100

    Preferred language segment use hot_contact table

--- sql/contact_segment_option.sql
+++ sql/contact_segment_option.sql
@@ -2,7 +2,11 @@
 -- DELETE cs FROM contact_segment cs JOIN segment s ON cs.segment_id = s.id JOIN segmentation sn ON sn.id = s.segmentation_id WHERE sn.external_system = 'civicrm_option_group' AND sn.name != 'Active status'
 
 -- BEGIN INCREMENTAL
-DELETE cs FROM contact_segment cs JOIN segment s ON cs.segment_id = s.id JOIN segmentation sn ON sn.id = s.segmentation_id WHERE sn.external_system = 'civicrm_option_group' AND sn.name = 'Preferred language';
+DELETE cs FROM contact_segment cs
+          JOIN segment s ON cs.segment_id = s.id
+          JOIN segmentation sn ON sn.id = s.segmentation_id
+          JOIN hot_contact hc ON cs.contact_id = hc.id AND hc.modified
+          WHERE sn.external_system = 'civicrm_option_group' AND sn.name = 'Preferred language';
 -- END INCREMENTAL
 
 INSERT INTO contact_segment -- Preferred Language
@@ -13,5 +17,8 @@ INSERT INTO contact_segment -- Preferred Language
   JOIN segment s ON s.segmentation_id = sn.id
   JOIN ${SOURCE}.civicrm_option_value ov ON s.external_id = ov.id
   JOIN contact c ON SUBSTRING(c.preferred_language, 1, 2) = ov.value COLLATE utf8_general_ci
+-- BEGIN INCREMENTAL
+   JOIN hot_contact hc ON c.id = hc.id AND hc.modified
+-- END INCREMENTAL
   WHERE sn.name = 'Preferred language'
 ;
