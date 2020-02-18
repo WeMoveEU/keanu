70cb8964bd185f4796630e472dc29088de713a50
Marcin Koziej <marcin@cahoots.pl>
2020-02-18 16:44:55 +0100

    Add id identity with source in broadcast, broadcast_test and campaign

--- sql/broadcast.sql
+++ sql/broadcast.sql
@@ -2,8 +2,9 @@
 -- DELETE FROM broadcast;
 
 INSERT INTO broadcast 
-  (name, broadcast_type, sent_at, broadcast_test_id, campaign_id, external_id, external_system)
+  (id, name, broadcast_type, sent_at, broadcast_test_id, campaign_id, external_id, external_system)
   SELECT
+    m.id, -- identity with source
     m.name, 'email', scheduled_date, t.id, camp.id, m.id, 'civicrm_mailing'
   FROM ${SOURCE}.civicrm_mailing m
   JOIN ${SOURCE}.civicrm_campaign c ON c.id=m.campaign_id
--- sql/broadcast_test.sql
+++ sql/broadcast_test.sql
@@ -1,8 +1,9 @@
 -- ORDER: 50
 -- DELETE FROM broadcast_test
 
-INSERT INTO broadcast_test (name, created_at, sent_at, external_id, external_system)
+INSERT INTO broadcast_test (id, name, created_at, sent_at, external_id, external_system)
   SELECT
+    ab.id, -- identity with the source
     ab.name, ab.created_date, ma.scheduled_date, ab.id, 'civicrm_mailing_abtest'
   FROM ${SOURCE}.civicrm_mailing_abtest ab
   JOIN ${SOURCE}.civicrm_mailing ma ON ma.id=ab.mailing_id_a
