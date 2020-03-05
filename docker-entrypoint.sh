4a20e5e2eb77e3d5873f7025d085293b4ac8adfa
Marcin Koziej <marcin@cahoots.pl>
2020-03-05 13:57:56 +0100

    Simplified the entry point to restart on success, exit on failure.
    The restart mode is enabled if RESTART env var is set.
    Otherwise this is just one-off execution of the command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,40 +4,17 @@ set -u
 set -o pipefail # set $? to first non zero exit value
                 # so we know when keanu failed when we pipe it to tee
 
-trap 'exit 0' TERM
+trap 'exit 0' TERM # Okay exit on docker stop
 
-if [ "$1" = "-r" ]; then
-    RESTART=1;
-    shift
-else
-    RESTART=0
-fi
-
-FAIL_WAIT=0
-
-if [ $RESTART -eq 1 ]; then
+if [ -n "${RESTART:-}" ]; then
     while true; do
         "$@" 2>&1 | tee /tmp/keanu.log
+
         if [ $? -gt 0 ]; then
-            # error
-            aborted_error=$(grep -B 1 Aborted /tmp/keanu.log)
-            traceback_error=$(grep -A 1000 '^Traceback' /tmp/keanu.log)
-            if [ -n "$aborted_error" -o -n "$traceback_error" ]; then
-                echo "REPORTING ERROR TO ENDPOINT?"
-                if [ -n "$aborted_error" ]; then 
-                    echo "$aborted_error"
-                fi
-                if [ -n "$traceback_error" ]; then
-                    echo "$traceback_error"
-                fi
-            fi
-            FAIL_WAIT=$(printf "%d" $(( ($FAIL_WAIT + 5) * 110 / 100 )))
-            echo $(printf "Sleeping for %ds" $FAIL_WAIT)
-            sleep $FAIL_WAIT
-        else
-            FAIL_WAIT=0
+            # Fail exit when our command fails
+            exit 1
         fi
     done
-else
+else # just a normal one-off run
     exec "$@"
 fi
