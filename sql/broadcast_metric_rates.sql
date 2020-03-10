44c49c04df1260342dea7e9ab3c4fa9f76d58391
Marcin Koziej <marcin@cahoots.pl>
2020-03-10 09:53:28 +0100

    Name the metrics based on primary metric (openERS->openERS_rate etc)

--- sql/broadcast_metric_rates.sql
+++ sql/broadcast_metric_rates.sql
@@ -1,10 +1,10 @@
 -- ORDER: 57
--- DELETE FROM broadcast_metric WHERE metric IN ('open_rate', 'click_rate', 'share_rate', 'forward_rate')
+-- DELETE FROM broadcast_metric WHERE metric IN ('openers_rate', 'clickers_rate', 'sharers_rate', 'likely_forwarders_rate')
 
 
 INSERT INTO broadcast_metric (broadcast_id, broadcast_name, segment_id, metric, value)
 SELECT
-  b1.broadcast_id, b1.broadcast_name, b1.segment_id, 'open_rate',
+  b1.broadcast_id, b1.broadcast_name, b1.segment_id, 'openers_rate',
   b1.value / b2.value
   FROM broadcast_metric b1
          JOIN broadcast_metric b2 ON b1.broadcast_id = b2.broadcast_id
@@ -17,7 +17,7 @@ SELECT
 
 INSERT INTO broadcast_metric (broadcast_id, broadcast_name, segment_id, metric, value)
 SELECT
-  b1.broadcast_id, b1.broadcast_name, b1.segment_id, 'click_rate',
+  b1.broadcast_id, b1.broadcast_name, b1.segment_id, 'clickers_rate',
   b1.value / b2.value
   FROM broadcast_metric b1
          JOIN broadcast_metric b2 ON b1.broadcast_id = b2.broadcast_id
@@ -29,19 +29,7 @@ SELECT
 
 INSERT INTO broadcast_metric (broadcast_id, broadcast_name, segment_id, metric, value)
 SELECT
-  b1.broadcast_id, b1.broadcast_name, b1.segment_id, 'click_rate',
-  b1.value / b2.value
-  FROM broadcast_metric b1
-         JOIN broadcast_metric b2 ON b1.broadcast_id = b2.broadcast_id
-             AND b1.metric = 'clickers' AND b2.metric = 'recipients'
-             AND b1.segment_id = b2.segment_id
-             ON DUPLICATE KEY UPDATE value=VALUES(value)
-         ;
-
-
-INSERT INTO broadcast_metric (broadcast_id, broadcast_name, segment_id, metric, value)
-SELECT
-  b1.broadcast_id, b1.broadcast_name, b1.segment_id, 'share_rate',
+  b1.broadcast_id, b1.broadcast_name, b1.segment_id, 'sharers_rate',
   b1.value / b2.value
   FROM broadcast_metric b1
          JOIN broadcast_metric b2 ON b1.broadcast_id = b2.broadcast_id
@@ -53,7 +41,7 @@ SELECT
 
 INSERT INTO broadcast_metric (broadcast_id, broadcast_name, segment_id, metric, value)
 SELECT
-  b1.broadcast_id, b1.broadcast_name, b1.segment_id, 'forward_rate',
+  b1.broadcast_id, b1.broadcast_name, b1.segment_id, 'likely_forwarders_rate',
   b1.metric / b2.metric
   FROM broadcast_metric b1
          JOIN broadcast_metric b2 ON b1.broadcast_id = b2.broadcast_id
