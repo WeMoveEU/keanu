17917916aa5f2948adbdcd2aa55d2631928bef5b
Romain Thouvenin <romain@wemove.eu>
2020-04-02 14:45:54 +0200

    Fix contact_segment for countries #68

--- sql/contact_segment_country.sql
+++ sql/contact_segment_country.sql
@@ -6,13 +6,12 @@ DELETE cs FROM contact_segment cs JOIN segment s ON cs.segment_id = s.id JOIN se
 -- END INCREMENTAL
 
 INSERT INTO contact_segment -- Country
-(segmentation_id, segment_id, contact_id, joined_at, left_at)
-SELECT
-s.segmentation_id, s.id,
-c.id, c.created_at, NULL
-FROM
-segmentation sn JOIN segment s ON s.segmentation_id = sn.id
-JOIN ${SOURCE}.civicrm_country ctr ON s.external_id = ctr.id
-JOIN contact c ON c.country = ctr.iso_code COLLATE utf8_general_ci
-WHERE sn.name = 'Country'
+  (segmentation_id, segment_id, contact_id, joined_at, left_at)
+
+  SELECT
+    s.segmentation_id, s.id, c.id, c.created_at, NULL
+  FROM segmentation sn
+  JOIN segment s ON s.segmentation_id = sn.id
+  JOIN contact c ON c.country = s.name
+  WHERE sn.name = 'Country'
 ;
