fd00c53fa511ac2de22948add3323f24b12b73ba
Marcin Koziej <marcin@cahoots.pl>
2020-04-07 11:35:54 +0200

    Update country segment generation

--- sql/contact_segment_country.sql
+++ sql/contact_segment_country.sql
@@ -1,9 +1,9 @@
 -- ORDER: 67
 -- DELETE cs FROM contact_segment cs JOIN segment s ON cs.segment_id = s.id JOIN segmentation sn ON sn.id = s.segmentation_id WHERE sn.name = 'Country'
 
--- BEGIN INCREMENTAL
-DELETE cs FROM contact_segment cs JOIN segment s ON cs.segment_id = s.id JOIN segmentation sn ON sn.id = s.segmentation_id WHERE sn.name = 'Country';
--- END INCREMENTAL
+SET @max_modified_date := (SELECT max(modified_date) FROM ${SOURCE}.civicrm_contact); 
+SET @last_update_dt := (SELECT last_sync_dt('contact_segment.country', 'civicrm_contact'));
+
 
 INSERT INTO contact_segment -- Country
 (segmentation_id, segment_id, contact_id, joined_at, left_at)
@@ -13,6 +13,15 @@ c.id, c.created_at, NULL
 FROM
 segmentation sn JOIN segment s ON s.segmentation_id = sn.id
 JOIN ${SOURCE}.civicrm_country ctr ON s.external_id = ctr.id
-JOIN contact c ON c.country = ctr.iso_code COLLATE utf8_general_ci
-WHERE sn.name = 'Country'
+JOIN ${SOURCE}.civicrm_contact c ON c.country = ctr.iso_code COLLATE utf8_general_ci
+ WHERE sn.name = 'Country'
+  -- BEGIN INCREMENTAL
+  AND c.modified_date > @last_update_dt
+  -- END INCREMENTAL
+
+ON DUPLICATE KEY UPDATE segment_id = s.id
 ;
+
+
+SELECT save_last_sync_dt('contact_segment.country', 'civicrm_contact',
+                         (SELECT max(modified_date) FROM ${SOURCE}.civicrm_contact));
