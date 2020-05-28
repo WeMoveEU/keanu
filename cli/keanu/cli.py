2ffdc44194bd79dc73c67dc182db90c89b22fcac
Marcin Koziej <marcin@cahoots.pl>
2020-05-28 15:39:38 +0200

    Use -j -y switches in metabase import/export

--- cli/keanu/cli.py
+++ cli/keanu/cli.py
@@ -200,31 +200,38 @@ def metabase_import(collection, json_file, metadata, overwrite, db_map, validate
     client = metabase.Client()
     mio = metabase.MetabaseIO(client)
     db_mapping = {d1: d2 for (d1,d2) in map(lambda x: x.split(":"), db_map)}
-    with open(json_file, 'r') as f:
-        source = json.loads(f.read())
-        if validate:
-            if verbose:
-                click.echo("🔍 Validating import file coherence...")
-            broken_cards = metabase.broken_cards(source['items'], source['datamodel'])
-            if len(broken_cards) > 0:
-                print("There are broken cards:")
-                for bc in broken_cards:
-                    print("{}: {}".format(*bc))
+    if json_file:
+        with open(json_file, 'r') as f:
+            source = json.loads(f.read())
+    elif yaml_dir:
+        splitter = metabase.Splitter(yaml_dir)
+        source = splitter.load()
+    else:
+        raise click.Abort("You need to specify json file with -j or yaml directory with -y")
 
-            broken_dashboards = metabase.broken_dashboards(source['items'])
-            if len(broken_dashboards) > 0:
-                print("There are broken dashboards (with questions outside of imported collection):")
-                for bd in broken_dashboards:
-                    print("{}: {} (missing card id {})".format(*bd))
+    if validate:
+        if verbose:
+            click.echo("🔍 Validating import file coherence...")
+        broken_cards = metabase.broken_cards(source['items'], source['datamodel'])
+        if len(broken_cards) > 0:
+            print("There are broken cards:")
+            for bc in broken_cards:
+                print("{}: {}".format(*bc))
 
-            broken_datamodel = metabase.broken_datamodel(source['datamodel'])
-            if len(broken_datamodel) > 0:
-                print("This is broken in the data model:")
-                for bd in broken_datamodel:
-                    print("{}: {} - {}".format(*bd))
+        broken_dashboards = metabase.broken_dashboards(source['items'])
+        if len(broken_dashboards) > 0:
+            print("There are broken dashboards (with questions outside of imported collection):")
+            for bd in broken_dashboards:
+                print("{}: {} (missing card id {})".format(*bd))
 
-            if len(broken_cards) > 0 or len(broken_dashboards) > 0 or len(broken_datamodel) > 0:
-                return 1
+        broken_datamodel = metabase.broken_datamodel(source['datamodel'])
+        if len(broken_datamodel) > 0:
+            print("This is broken in the data model:")
+            for bd in broken_datamodel:
+                print("{}: {} - {}".format(*bd))
+
+        if len(broken_cards) > 0 or len(broken_dashboards) > 0 or len(broken_datamodel) > 0:
+            return 1
 
 
         mio.import_json(source, collection, metadata,
