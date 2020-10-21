1b5056befe802cc2a0b98fde91f74a6e534ef011
Aaron Elliot Ross <aaronelliotross@birdhouse.paris>
2020-10-21 11:46:00 +0200

    Remove dotenv - pipenv already loads .env

--- keanu/__init__.py
+++ keanu/__init__.py
@@ -1,8 +1,3 @@
-import dotenv
-
-dotenv.load_dotenv()
-
-# pylint: disable=wrong-import-position
 from . import util
 from . import tracing
 from .test import BatchTestCase
--- keanu/cli.py
+++ keanu/cli.py
@@ -1,7 +1,3 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
 import click
 from click_aliases import ClickAliasedGroup
 import json
